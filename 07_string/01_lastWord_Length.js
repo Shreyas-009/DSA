b62c204078941a2a878f350cd791b89be06b8123
Shreyas <shreyastungar762@gmail.com>
2026-06-27 22:48:32 +0530

    58. Length of Last Word learned to solve using 3 different approaches

--- 07_string/01_lastWord_Length.js
+++ 07_string/01_lastWord_Length.js
@@ -1,17 +1,75 @@
-let str = "Hello   World   ";
+let str = "   fly me   to   the moon  ";
 
-function findLastWordLength(s) {
+
+// approach 1: using inbuild methodes of string
+function findLastWordLength1(s) {
   let arr = s.split(" ");
   let fill = "";
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i]){
-        fill = arr[i];
-        // console.log("i",arr[i]);
-        // console.log("fill",arr[fill]);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+      fill = arr[i];
+      // console.log("i",arr[i]);
+      // console.log("fill",arr[fill]);
     }
   }
-//   console.log(fill);
+  //   console.log(fill);
   return fill.length;
 }
 
-console.log(findLastWordLength(str));
+
+// approach 2 : using loop and cound from last by ignoring white spaces
+function findLastWordLength2(s) {
+  let finalString = 0;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] != " ") {
+      finalString++;
+    } else {
+      if (finalString && s[i] == " ") {
+        break;
+      } else {
+        finalString = 0;
+      }
+    }
+  }
+
+  return finalString;
+}
+
+// approach 3 : removing extra white spaces manuall trimming then finding last word and return its length
+function findLastWordLength3(s) {
+  let finalString = "";
+  let word = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] != " ") {
+      finalString += s[i];
+      word = s[i];
+    } else {
+      if (s[i] != word) {
+        finalString += s[i];
+      }
+      word = s[i];
+    }
+  }
+
+//   console.log(finalString);
+
+  let lastWord = "";
+  for (let i = 0; i < finalString.length - 1; i++) {
+    if (finalString[i] != " ") {
+      lastWord += finalString[i];
+    } else {
+        if (finalString[i+1] != " ") lastWord = "";
+    }
+  }
+
+  return lastWord.length;
+}
+
+
+
+
+console.log(findLastWordLength3(str));
+console.log(findLastWordLength2(str));
+console.log(findLastWordLength1(str));
+
